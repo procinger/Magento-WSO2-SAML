a330f86fe1cb6ac0a44b534ba75d9e982a98b4a5
procinger <procing3r@gmail.com>
2017-04-22 15:33:46 +0200

    update wso_scim_id used forms

---
+++ src/app/code/community/Hukmedia/Wso2/Block/Config/Adminhtml/Form/Field/Customer/Attributes.php
@@ -0,0 +1,8 @@
+<?php
+
+class Hukmedia_Wso2_Block_Config_Adminhtml_Form_Field_Customer_Atributes extends Mage_Core_Block_Html_Select
+{
+    public function _toHtml() {
+        return parent::toHtml();
+    }
+}
---
+++ src/app/code/community/Hukmedia/Wso2/Model/Adminhtml/System/Config/Source/Claim.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: procinger
+ * Date: 22.04.17
+ * Time: 11:46
+ */
