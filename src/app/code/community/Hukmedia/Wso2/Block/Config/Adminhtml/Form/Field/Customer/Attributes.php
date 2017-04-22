0fa49432b7d212f922d87a5bcd9affb8b3148c25
procinger <procing3r@gmail.com>
2017-04-22 16:48:33 +0200

    claim management to backend added

--- src/app/code/community/Hukmedia/Wso2/Block/Config/Adminhtml/Form/Field/Customer/Attributes.php
+++ src/app/code/community/Hukmedia/Wso2/Block/Config/Adminhtml/Form/Field/Customer/Attributes.php
@@ -1,8 +1,20 @@
 <?php
 
-class Hukmedia_Wso2_Block_Config_Adminhtml_Form_Field_Customer_Atributes extends Mage_Core_Block_Html_Select
+class Hukmedia_Wso2_Block_Config_Adminhtml_Form_Field_Customer_Attributes extends Mage_Core_Block_Html_Select
 {
-    public function _toHtml() {
-        return parent::toHtml();
+    protected function _toHtml() {
+        $addressForm = Mage::getSingleton('customer/form');
+        $addressForm->setFormCode('customer_account_create');
+        $attributes = $addressForm->getAttributes();
+
+        foreach($attributes as $option) {
+            $this->addOption($option->getAttributeCode(), $option->getFrontendLabel());
+        }
+
+        return parent::_toHtml();
+    }
+
+    public function setInputName($value) {
+        return $this->setName($value);
     }
 }
